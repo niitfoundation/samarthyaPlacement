710f6ac90e11cbaabe336d44e61f44f9d61de631
pankushmanchanda <manchandapankush@gmail.com>
2017-05-19 11:43:56 +0530

    Component less Authgaurd routing

--- services/auth.guard.ts
+++ services/auth.guard.ts
@@ -1,33 +1,35 @@
 import { Injectable } from '@angular/core';
-import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, CanActivateChild } from '@angular/router';
 import { Headers, Http, Response, RequestOptions } from '@angular/http';
 import { Data } from './data.service';
-import {AuthenticationService} from './authentication.service'
+import { AuthenticationService } from './authentication.service' // http request using observable
+import 'rxjs/add/operator/map'
 // auhentication gaurd if without login user will try to access differnt menu links navigate it to login page
 
 @Injectable()
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivateChild {
 
-    constructor(private router: Router, private data: Data, private http: Http, private AuthenticationService:AuthenticationService) { }
-    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-
+    constructor(private router: Router, private data: Data, private http: Http, private AuthenticationService: AuthenticationService) { }
+    canActivateChild(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
         if (localStorage.getItem('currentUser')) {
             // logged in user return true
             return this.http.post('/authenticateToken', { token: this.AuthenticationService.getToken() })
-                .map((response: Response) => {
-                    this.AuthenticationService.setToken(response.json().authToken);
-                    return true;
-                },(err:any) => {
-                    console.log("eerrrr")
-                      this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
-                 this.data.openSnackBar('Please Login!!',"OK");
-                     return false;
+                .toPromise()
+                .then(response => {
+                    if (response.json().success != false) {
+                        this.AuthenticationService.setToken(response.json().authToken);
+                        return true;
+                    }
+                }).catch(response => {
+                    this.router.navigate(['/login']);
+                    this.data.openSnackBar('Please Login!!', 'OK');
+                    return false;
                 });
         }
         else {
             // not logged in so redirect to login page with the return url
             this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
-             this.data.openSnackBar('Please Login!!',"OK");
+            this.data.openSnackBar('Please Login!!', 'OK');
             return false;
         }
     }
